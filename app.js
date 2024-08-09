22b5cea1ad7c8929227347a6fb171e4e4e962b8d
DRedDevil04 <devamdesai@gmail.com>
2024-08-10 01:37:18 +0530

    changed frontend url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ dotenv.config();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
-  origin: 'https://campus-cart-frontend.vercel.app',
+  origin: 'https://takeiteasy-iiita.vercel.app',
   methods: ['GET', 'POST', 'OPTIONS', 'PUT'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
